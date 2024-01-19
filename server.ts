b4bda05fa2170f13de1e74a9cff5a3e3c6d89a3e
TeaChanathip <97622259+TeaChanathip@users.noreply.github.com>
2024-01-19 21:05:26 +0700

    add CORS

--- server.ts
+++ server.ts
@@ -1,7 +1,15 @@
 import express from "express";
+import cors from "cors";
 
 export const app = express();
 const port = 3000; // Change this if you want to run the server on a different port
+
+app.use(
+    cors({
+        origin: "*",
+        methods: ["GET"], 
+    })
+);
 
 require("./requests/restaurantRequest");
 require("./requests/menusRequest");
